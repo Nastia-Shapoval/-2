2741a3e6bde956daca91e488f471a06fed3c08b8
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-03-28 12:12:45 +0200

    Add files via upload

---
+++ лаба 2/лаб 2.4/лаб 2.4.cpp
@@ -0,0 +1,16 @@
+﻿#include <stdio.h>
+
+int main() {
+    double d1 = 120, t1 = 2; 
+    double d2 = 80, t2 = 1;  
+    double total_d, total_t, avg_speed;
+
+    total_d = d1 + d2;
+    total_t = t1 + t2;
+
+    avg_speed = total_d / total_t;
+
+    printf("Average speed: %.2lf km/h (as a fraction: %.0lf/%.0lf)\n", avg_speed, total_d, total_t);
+
+    return 0;
+}
