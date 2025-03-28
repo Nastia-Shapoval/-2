7095eb77b4deaf7ee6b61d6b271901c40129ff81
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-03-28 11:39:36 +0200

    Add files via upload

---
+++ лаба 2/лаб 2.3/лаб 2.3.cpp
@@ -0,0 +1,13 @@
+﻿#include <stdio.h>
+
+int main() {
+    int number;
+
+    printf("Enter an integer: ");
+    scanf_s("%d", &number);
+
+    printf("Last digit: %d\n", number % 10);
+
+    return 0;
+}
+
