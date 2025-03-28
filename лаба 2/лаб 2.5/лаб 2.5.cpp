b05f617153eddbd1e33a97ecbf3b6bd059ab2b01
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-03-28 12:13:20 +0200

    Add files via upload

---
+++ лаба 2/лаб 2.5/лаб 2.5.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main() {
+    int num;
+
+    // Зчитуємо тризначне число
+    printf("Enter a three-digit number: ");
+    scanf_s("%d", &num);
+
+    // Виводимо число у зворотному порядку
+    printf("Reversed number: %d\n", (num % 10) * 100 + ((num / 10) % 10) * 10 + (num / 100));
+
+    return 0;
+}
