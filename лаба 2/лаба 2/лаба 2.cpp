74cb61484f07c9dcef3eb81ba70b2f2029c369f8
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-03-28 11:13:42 +0200

    Add files via upload

---
+++ лаба 2/лаба 2/лаба 2.cpp
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+
+int main() {
+    int a, b;
+    printf("Enter two numbers -  ");
+    scanf_s("%d %d", &a, &b);
+
+    a = a + b;
+    b = a - b;
+    a = a - b;
+
+    printf("After swapping: a = %d, b = %d\n", a, b);
+    return 0;
+}
