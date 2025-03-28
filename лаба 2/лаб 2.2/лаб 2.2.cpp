6b745729120a6b845aa77d0638f62ba903c4c9df
Nastia-Shapoval <148361310+Nastia-Shapoval@users.noreply.github.com>
2025-03-28 11:31:44 +0200

    Add files via upload

---
+++ лаба 2/лаб 2.2/лаб 2.2.cpp
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+
+int main() {
+    int quantity;
+    double price, total;
+
+    printf("Enter the number of items: ");
+    scanf_s("%d", &quantity);
+
+    printf("Enter the price per unit: ");
+    scanf_s("%lf", &price);
+
+    total = quantity * price;
+
+    printf("Total purchase cost: %.2lf\n", total);
+
+    return 0;
+}
